cffee524aac998041492687e8184f6cb9eafd915
Jack Hare <jack.hare@hotmail.com>
2024-03-30 13:35:17 -0500

    Changed test_sample.py to purposely fail to test github actions autotesting

--- 2022_Packet/test_sample.py
+++ 2022_Packet/test_sample.py
@@ -11,6 +11,6 @@ def test_problem3():
     assert p3.validateAddress("A01F:0011:5CD3:E321:CCCC:FFFF:0000:0122") == True
     assert p3.validateAddress("127.0.0.1") == False
     assert p3.validateAddress("A01F:11:5CD3:E321:CCCC:FFFF:0:122") == True
-    assert p3.validateAddress("A01F:11:5CD3:E321:CCCC:FFFF::122") == True
+    assert p3.validateAddress("A01F:11:5CD3:E321:CCCC:FFFF::122") == False
     assert p3.validateAddress("C23:53:2::FF:3456:3D:483") == True
     assert p3.validateAddress("34.23.13456.3245H.403.7457.2.3DEFF") == False
